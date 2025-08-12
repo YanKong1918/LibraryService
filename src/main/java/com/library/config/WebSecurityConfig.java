39586b5ebfef70f176a3c05e4d51c49031a1a1f3
hyome <hyome@nbware.co.kr>
2025-08-12 11:48:28 +0900

    :ambulance: WebSecurityConfig 클래스 수정.. 어떻게 이런 실수를... 모든 api 요청이 401 Unauthorized 로 무시되는 현상 해결 -> @Configuration: 해당 설정 클래스를 bean으로 등록하기 위해 필요 @EnableWebSecurity: 스프링 시큐리티 기능 활성화.. => 해당 어노테이션이 누락될 경우 SecurityFilterChain에서 설정한 보안 설정 내용들이 싸그리 무시됨...

--- src/main/java/com/library/config/WebSecurityConfig.java
+++ src/main/java/com/library/config/WebSecurityConfig.java
@@ -1,12 +1,16 @@
 package com.library.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.filter.CharacterEncodingFilter;
 
+@Configuration
+@EnableWebSecurity
 public class WebSecurityConfig {
 
     @Bean
